e397dc6e787aa4b746d35189ae4ae50a161ea7c1
dmytrokniahnytskyi <dimak911@gmail.com>
2023-02-21 21:09:25 +0200

    add test form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,61 @@
-const App = () => {
+import { FC, useContext, useEffect, useState } from "react";
+import { Context } from "./index";
+import LoginForm from "./components/LoginForm";
+import { observer } from "mobx-react-lite";
+import { IUser } from "./models/IUser";
+import UserService from "./services/UserService";
+
+const App: FC = () => {
+  const { store } = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      store.checkAuth();
+    }
+  }, [store]);
+
+  const getUsers = async () => {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  if (store.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!store.isAuth) {
+    return (
+      <>
+        <LoginForm />
+        <button onClick={getUsers}>Get users</button>
+      </>
+    );
+  }
+
   return (
-    <div>
-      <h1>Works!</h1>
-    </div>
+    <>
+      <h2>
+        {store.isAuth ? `User is authorized ${store.user.email}` : "Authorize"}
+      </h2>
+      <h2>
+        {store.user.isActivated
+          ? "Account activated"
+          : "Account not activated. Activate your account please."}
+      </h2>
+      <button onClick={() => store.logout()}>Logout</button>
+
+      <button onClick={getUsers}>Get users</button>
+
+      {users.map((user) => (
+        <div key={user.email}>{user.email}</div>
+      ))}
+    </>
   );
 };
 
-export default App;
+export default observer(App);
